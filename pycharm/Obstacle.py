5d2a3f01761c16ff696670af91003c4dcd9d3bec
lukardvn <rluka996@gmail.com>
2018-12-26 16:08:28 +0100

    Obstacle
    car

---
+++ pycharm/Obstacle.py
@@ -0,0 +1,21 @@
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLabel
+from Rectangle import Rectangle
+from Config import Config
+
+
+class Obstacle(Rectangle):
+    speed = 0
+
+    def __init__(self, x, y, w, h, s):
+        super().__init__(x, y, w, h)
+        self.sprite = 'car_obstacle.png'
+        self.speed = s
+
+    def update(self):
+        self.x = self.x + self.speed
+
+        if self.speed > 0 and self.x > Config.gridSize:
+            x = -self.w - Config.gridSize
+        elif self.speed < 0 and self.x < Config.gridSize:
+            x = self.w + Config.gridSize
