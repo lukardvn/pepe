173ac01c813b17f8ca9e133394913e9a90475f1f
djape007 <bpredrag96@gmail.com>
2018-12-29 17:56:36 +0100

    definicije za lejnove easy, medium, hard

--- pycharm/Config.py
+++ pycharm/Config.py
@@ -46,6 +46,7 @@ class Config:
         "car_1": 100,
         "car_2": 100,
         "car_3": 100,
+        "car_4": 107,
         "truck_1": 200,
         "truck_2": 135
     }
@@ -60,3 +61,54 @@ class Config:
         "log_medium": 158,
         "log_large": 310
     }
+
+    #tupla => broj prepreka, brzina, spejsing, tipLejna
+    lanesEasyConfig = [
+        (1, 5.6, 180, laneTypeTraffic),
+        (8, 3.9, 210, laneTypeTraffic),
+        (3, 4.5, 240, laneTypeTraffic),
+        (12, 3.2, 170, laneTypeTraffic),
+    ]
+
+    #tupla => broj prepreka, brzina, spejsing, tipLejna
+    lanesMediumConfig = [
+        (3, 6, 200, laneTypeTraffic),
+        (8, 4.7, 240, laneTypeTraffic),
+        (6, 8, 280, laneTypeTraffic),
+        (15, 5.2, 200, laneTypeTraffic),
+        (20, 6.25, 220, laneTypeTraffic),
+        (10, 9, 450, laneTypeTraffic),
+        (6, 6.3, 240, laneTypeTraffic)
+    ]
+
+    #tupla => broj prepreka, brzina, spejsing, tipLejna
+    lanesHardConfig = [
+        (5, 8, 175, laneTypeTraffic),
+        (1, 15, 175, laneTypeTraffic),
+        (7, 9, 240, laneTypeTraffic),
+        (13, 6, 185, laneTypeTraffic),
+        (4, 11, 270, laneTypeTraffic),
+        (3, 7.33, 110, laneTypeTraffic),
+        (2, 13.2, 95, laneTypeTraffic)
+    ]
+
+    # tupla => broj prepreka, brzina, spejsing, tipLejna
+    lanesEasyWaterConfig = [
+        (4, 3.2, 150, laneTypeWater),
+        (3, 4.7, 220, laneTypeWater),
+        (2, 4, 250, laneTypeWater)
+    ]
+
+    # tupla => broj prepreka, brzina, spejsing, tipLejna
+    lanesMediumWaterConfig = [
+        (6, 5.3, 260, laneTypeWater),
+        (4, 6.3, 280, laneTypeWater),
+        (3, 7, 230, laneTypeWater)
+    ]
+
+    # tupla => broj prepreka, brzina, spejsing, tipLejna
+    lanesHardWaterConfig = [
+        (3, 7.32, 315, laneTypeWater),
+        (4, 8.23, 320, laneTypeWater),
+        (5, 9.15, 260, laneTypeWater)
+    ]
