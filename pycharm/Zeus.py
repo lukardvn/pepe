a73bedbc3a8f38247bc6dedf65e98ee802e724b8
djape007 <bpredrag96@gmail.com>
2019-01-10 20:36:53 +0100

    obrisano sve sto se ne koristi

--- pycharm/Zeus.py
+++ pycharm/Zeus.py
@@ -4,7 +4,6 @@ import multiprocessing as mp
 from multiprocessing import Queue
 
 class Zeus:
-    oneInstance = None
 
     def __init__(self, kju, secondsSunnyMin, secondsSunnyMax, secondsRainMin, secondsRainMax):
         self.minSunny = secondsSunnyMin
@@ -12,26 +11,21 @@ class Zeus:
         self.minRain = secondsRainMin
         self.maxRain = secondsRainMax
         self.kju = kju
-        self.radi = True
         self.vreme = "s"    #kakvo je trenutno vreme; n=>NISTA (nema ni snega ni kise), s=>sneg, k=>kisa
-        Zeus.oneInstance = self
 
-    def stop(self):
-        self.radi = False
 
     def MakeItRain(self):
-        while self.radi:
+        while True:
             self.ChangeWheather()
             if self.vreme == "n":
                 time.sleep(random.random() * self.maxSunny + self.minSunny)
-            elif self.vreme == 's':
-                time.sleep(random.random() * self.maxRain + self.minRain)
-            elif self.vreme == 'k':
+            elif self.vreme == 's' or self.vreme == 'k':
                 time.sleep(random.random() * self.maxRain + self.minRain)
 
     def ChangeWheather(self):
         self.vreme = random.choice(['s', 'n', 'k', 'n', 'n', 'n']) #da budu vece sanse da je suncano nego kisa/sneg
         self.kju.put(self.vreme)
+
 
 def _zevseOvoRadi(kju):
     zevs = Zeus(kju, 3, 7, 1, 3)
